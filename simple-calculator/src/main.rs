f449a931bd9fd779fd57f2a18c5f9c531702601c
Bikathi Martin <bikathimartin@outlook.com>
2023-12-23 18:21:23 +0300

    First commit

--- simple-calculator/src/main.rs
+++ simple-calculator/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("WELCOME TO THIS SIMPLE CALCULATOR");
+
+    println!("Enter your first number: ");
+    let mut first_number: String = String::new();
+
+    io::stdin()
+        .read_line(&mut first_number)
+        .expect("Failed to read your input");
+
+    let first_number: u32 = first_number.trim().parse().expect("Input is NaN");
+
+    println!("Entered first_number is: {first_number}");
 }
